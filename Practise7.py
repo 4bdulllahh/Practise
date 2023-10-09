dc76d3a45bd610d143c826e8b9ee3a2271f012c8
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-09 19:26:09 +0400

    .

---
+++ Practise7.py
@@ -0,0 +1,10 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+gender = input("What gender are you?: ")
+
+if age < 18:
+    print("Sorry you are not allowed to enter")
+elif age >= 18:
+    Print ("Welcome you may enter")
+
+
