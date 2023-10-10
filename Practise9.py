0da9d36cb68357352aa2ae3a57edc5d43dc78aac
Abdullah <abdullahkamran823@gmail.com>
2023-10-10 22:26:32 +0400

    Update Practise9.py

--- Practise9.py
+++ Practise9.py
@@ -1,26 +1,26 @@
 import turtle
 
-i=0  
+i=0
 while i <360:
-turtle.forward(20)
-turtle.left(15)
-i+=15
+    turtle.forward(20)
+    turtle.left(15)
+    i+=15
 i=0
 
 
 while i <450:
-turtle.forward(20)
-turtle.right(15)
-i+=15
+    turtle.forward(20)
+    turtle.right(15)
+    i+=15
 
-i=0
+i=0 
 turtle.left(90)
 turtle.forward(200)
 
 while i <90:
-turtle.forward(20)
-turtle.left(15)
-i+=15
+    turtle.forward(20)
+    turtle.left(15)
+    i+=15
 
 turtle.left(90)
 turtle.forward(30)
@@ -29,11 +29,11 @@ turtle.forward(30)
 turtle.left(90)
 
 while i <540:
-turtle.forward(20)
-turtle.left(15)
-i+=15
+    turtle.forward(20)
+    turtle.left(15)
+    i+=15
 i+=0
 
 turtle.forward(225)
 
-input()
+input()    
