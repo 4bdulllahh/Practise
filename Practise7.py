105216a2e35fbf000a4f965a14b959b0ddbda469
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-12 09:18:02 +0400

    .

--- Practise7.py
+++ Practise7.py
@@ -5,6 +5,6 @@ gender = input("What gender are you?: ")
 if age < 18:
     print("Sorry you are not allowed to enter")
 elif age >= 18:
-    Print ("Welcome you may enter")
+    print ("Welcome you may enter")
 
 
--- Practise8.py
+++ Practise8.py
@@ -1,5 +1,7 @@
 string1 = "Mariam"
 string2 = "is" 
-string3 = "gay" 
+string3 = "Gay" 
 output_string = string1 + string2 + string3 
 print("output_string")
+
+#Output will be -----> Mariam is Gay
