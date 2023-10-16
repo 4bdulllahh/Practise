496580baac1f48e2ac3a69541b5218f876c47c5e
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-16 10:14:52 +0400

    .

---
+++ List/Practise6.py
@@ -0,0 +1,2 @@
+list=[1,2,3,4,5,6,7,8,9,10,11,12,13]
+print("The amount of a numbers in the list:", len(list))
---
+++ List/Practise7.py
@@ -0,0 +1,2 @@
+list(1,2,23,6446,34,345,683,9,236,9086,457,98,124,8)
+print("Number of number in elements: ", lens(list))
