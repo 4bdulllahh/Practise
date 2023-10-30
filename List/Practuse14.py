c2e7ea79146b06ff2c5ab61f26283a30f9f38fd3
4bdulllahh <abdullahkamran823@gmail.com>
2023-10-30 10:05:35 +0400

    .

---
+++ List/Practuse14.py
@@ -0,0 +1,17 @@
+#1
+a = {}
+print(a)
+
+#2
+x = {"brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(x)    
+
+#3
+y = {"brand": "Nissan",
+  "model": "Skyline",
+  "year": 1998
+}
+print(y["model"])
