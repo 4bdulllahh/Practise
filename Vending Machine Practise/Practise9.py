e806a209b9b26308ef78d6af4058cb25bab363cc
Abdullah <abdullahkamran823@gmail.com>
2023-12-26 13:21:15 +0400

    .

---
+++ Vending Machine Practise/Practise9.py
@@ -0,0 +1,15 @@
+water = {"Al ain(01)":"1 dhs","Masafi(02)":"1 dhs","Oasis(03)":"1 dhs","Aquafina(04)":"1 dhs","Arwa(05)":"1 dhs"} ##The menu
+drinks = {"Coca cola(06)":" 2.5 dhs","7 up(07)":"2.5 dhs","Pepsi(08)":"2.5 dhs","Mountain dew(09)":"2.5 dhs","Fanta(10)":"2.5 dhs"} 
+jelly = {"Fruit jelly(11)":"0.5 dhs","Gummy bears(12)":"1 dhs","Jelly beans(13)":"5 dhs","Soda jelly(14)":"0.5 dhs","Sour jelly(15)":"1 dhs"}
+juices = {"Apple juice(16)":"2 dhs","Mango juice(17)":"2 dhs","Orange juice(18)":"2 dhs","Grape juice(19)":"2 dhs","Mix fruit juice(20)":"2 dhs"}
+chips_small = {"Lays(Salt)(21)":"1 dhs","Lays(Cheese)(22)":"1 dhs","Lays(Tomato)(23)":"1 dhs","Lays(Chilli)(24)":"1 dhs","Lays(Salt & Vinger)(25)":"1 dhs"}
+chips_big = {"takis(26)":"3 dhs","Doritos(27)":"3 dhs","Pringle(28)":"3 dhs","Buggles(29)":"3 dhs","Cheetos(30)":"3 dhs"}
+items = [water,drinks,jelly,juices,chips_small,chips_big]
+
+
+
+"Al ain(01)":"1 dhs"
+
+"Masafi(02)":"1 dhs
+
+"Oasis(03)":"1 dhs"
