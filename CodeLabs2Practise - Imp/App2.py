c8772cbc5ae91d5e74a2e666d51de4a8fd633a4d
4bdulllahh <abdullahkamran823@gmail.com>
2024-10-15 14:37:32 +0400

    .

---
+++ CodeLabs2Practise - Imp/App2.py
@@ -0,0 +1,10 @@
+from tkinter import *
+main = Tk()
+Label(main, text="Alpha",bg="White", fg="Red",
+font=("Arial", 20, "bold")).pack()
+main.mainloop() 
+
+b1 = Button(main, text="Beta",bg="Black", fg="white",
+font=("Arial", 20, "bold"))
+b1.pack()
+root.mainloop()
--- CodeLabs2Practise - Imp/FirstApp.py
+++ CodeLabs2Practise - Imp/FirstApp.py
@@ -1,4 +1,9 @@
 from tkinter import *
-
-
+main = Tk()
+main.title('Using Pack()')
+Button(main,text="A").pack(side=LEFT, expand=YES, fill=Y)
+Button(main,text="B",bd=15 ,bg="red", fg="white").pack(side=TOP, expand=YES, fill=BOTH)
+Button(main,text="C").pack(side=RIGHT, expand=YES, fill=NONE, anchor=NE,pady=6)
+Button(main,text="D").pack(side=BOTTOM, expand=NO, fill=Y,pady=6)
+main.mainloop()
 
